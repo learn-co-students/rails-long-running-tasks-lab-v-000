51c7bb0e163f6a0eef30466df2a063c6f646c1fd
Rawradin <edgar.rawr@gmail.com>
2016-04-05 02:15:44 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -64,10 +64,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
-  # def upload
-  #   CSV.foreach(params[:leads].path, headers: true) do |lead|
-  #     Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
-  #   end
-  #   redirect_to customers_path
-  # end
