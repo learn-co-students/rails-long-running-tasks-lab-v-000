fb3b93bbe086559296ffc4901fb7e17eb56e04a6
febbraiod <donedesign1@gmail.com>
2016-04-26 12:08:15 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  require 'csv'
 
   def artist_name
     self.try(:artist).try(:name)
@@ -9,4 +10,12 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
+  def self.upload(params)
+    CSV.foreach(params[:file].path, headers: true) do |song|
+      s = Song.create(title: song[0])
+      s.artist = Artist.find_or_create_by(name: song[1])
+    end
+  end
+
 end
