bc821ec29de135399f5a9867b12baae521a97d45
azheng249 <andy.zheng249@gmail.com>
2016-04-27 13:47:40 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
@@ -20,6 +21,15 @@ class SongsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def upload
+    CSV.foreach(params[:file].path, headers: true) do |file|
+      artist = Artist.find_or_create_by(name: file[1])
+      Song.find_or_create_by(title: file[0], artist: artist)
+    end
+
+    redirect_to songs_path
   end
 
   def edit
