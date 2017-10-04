b33dfe5a42b94e358236b7b9a3db802efa7c6ade
Will-Sauer <will@heueso.com>
2017-10-04 13:01:58 -0400

    done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-
+require 'CSV'
   def index
     @songs = Song.all
   end
@@ -38,6 +38,15 @@ class SongsController < ApplicationController
     end
   end
 
+  def upload
+   CSV.foreach(params["file"].path, headers: true) do |song|
+      artist = Artist.find_or_create_by(:name => song[1])
+      Song.create(title: song[0], artist: artist)
+    end
+    redirect_to songs_path
+  end
+  
+
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
@@ -48,7 +57,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :file)
   end
 end
 
