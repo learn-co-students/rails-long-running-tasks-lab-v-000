03ee1e62f77337277d51584c19205898616e8913
Archana Burra <archana@betterment.com>
2017-12-14 21:32:53 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
@@ -45,10 +46,18 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+  def upload
+    CSV.foreach(params[:file].path, headers: true) do |song|
+      new_song = Song.new(title: song[0])
+      new_song.artist = Artist.find_or_create_by(name: song[1])
+      new_song.save
+    end
+    redirect_to songs_path
+  end
+
   private
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
