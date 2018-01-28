3e7dcec6bb655d602e8bb4b257e1c75cf8e9677f
streamedLine <dovidlwtn@gmail.com>
2018-01-27 22:27:42 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,6 +18,7 @@ class SongsController < ApplicationController
     CSV.foreach(params[:file].path, headers: true) do |line|
       song = Song.create(title: line[0])
       song.artist = Artist.find_or_create_by(name: line[1])
+      song.save
     end
     redirect_to songs_path
   end
