25967e91dea6365bb14686df7714dcb925f1bfc2
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-01 19:21:53 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,17 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
   end
+
+  def upload
+    CSV.foreach(params["file"].path, headers: true) do |song|
+      @artist = Artist.find_or_create_by(name: song[1])
+      @song = Song.create(title: song[0], artist: @artist)
+    end 
+    redirect_to songs_path  
+  end 
 
   def show
     @song = Song.find(params[:id])
