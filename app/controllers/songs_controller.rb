a9a81d5d9279764c816375fe64e04e0616720456
jljardon <github email address>
2018-06-26 22:24:13 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,4 @@
 class SongsController < ApplicationController
-
   def index
     @songs = Song.all
   end
@@ -41,7 +40,14 @@ class SongsController < ApplicationController
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    flash[:notice] = "Song deleted."
+    flash[:notice] = 'Song deleted.'
+    redirect_to songs_path
+  end
+
+  def upload
+    CSV.foreach(params[:file].path, headers: true) do |song|
+      Song.create(artist_name: song[1], title: song[0])
+    end
     redirect_to songs_path
   end
 
@@ -51,4 +57,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
