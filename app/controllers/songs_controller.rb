f3cf911ecec6ac4062ab4e11d4255098715691ed
nptravis <nptravis@yahoo.com>
2018-08-15 18:33:48 +0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,6 +45,27 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+  def upload
+    CSV.foreach(params[:songs].path, headers: true) do |song|
+      artist = Artist.find_or_create_by(name: song[1])
+      Song.find_or_create_by(title: song[0], artist_id: artist.id) 
+    end 
+    redirect_to songs_path
+  end
+
+  # create_table "artists", force: :cascade do |t|
+  #   t.string   "name"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+
+  # create_table "songs", force: :cascade do |t|
+  #   t.string   "title"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  #   t.integer  "artist_id"
+  # end
+
   private
 
   def song_params
