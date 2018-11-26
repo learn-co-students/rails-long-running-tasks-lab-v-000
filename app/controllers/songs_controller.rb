ec12c53abb601b3800b6940028015875485a1cdf
roitblatari <roitblatari@gmail.com>
2018-11-26 14:30:14 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,10 +45,20 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+  def upload
+    # Song Clean0,ARTIST CLEAN1,Release Year2,COMBINED3,First?4,Year?5,PlayCount6,F*G7
+    # binding.pry
+    CSV.foreach(params["file"].path, headers: true) do |file|
+      Song.create(title: file[0], artist_name: file[1])
+      # EmailAddress,GivenName,Surname
+   #   Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+   end
+   redirect_to songs_path
+  end
+
   private
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
