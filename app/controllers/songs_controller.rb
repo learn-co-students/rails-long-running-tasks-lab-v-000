ed41a64987401f4ce98d25e54ffabbd9a89841f3
epic0448 <samuelogundiran@Yahoo.com>
2019-02-13 14:22:02 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,9 +9,8 @@ class SongsController < ApplicationController
     #  i = 0
 
     CSV.foreach(params[:file].path, headers: true) do |song|
-      
-      s = Song.create(title:song[0], artist_name:song[1] )
-      puts s.title
+      binding.pry
+     Song.create(title:song[0], artist_name:song[1] )
       # Song Clean,ARTIST CLEAN,Release Year,COMBINED,First?,Year?,PlayCount,F*G
 
     end
