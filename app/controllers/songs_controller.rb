c58d8c09ff8709ddcecc5388bcaee24a2ab6a486
ryanwevans <ryanwevans@icloud.com>
2019-03-19 17:14:44 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
 class SongsController < ApplicationController
   require 'csv'
+  require 'pry'
 
   def index
     @songs = Song.all
@@ -47,9 +48,9 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params[:songs].path, headers: true) do |song|
+    CSV.foreach(params[:file].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
-      Artist.create(name: song[1])
+      # Artist.create(name: song[1])
     end
     redirect_to songs_path
   end
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.describe SongsController do
   describe "file upload" do
     before do
