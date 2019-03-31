68d33a2eef5b91459732d0e03b5a112390134f81
Naomi Dennis <njdennis94@gmail.com>
2019-03-31 01:53:59 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,16 @@ class SongsController < ApplicationController
 
   def upload
      CSV.foreach(params[:file].path, headers: true) do |song|
-       Song.create!({title: song[0]})
+       new_song = Song.create({title: song[0]})
+       artist = nil
+       if Artist.where(name: song[1]).empty?
+          artist = Artist.create!({name: song[1]})
+          artist.save!
+       else
+          artist = Artist.find_by(name: song[1])
+       end
+       new_song.artist = artist
+       new_song.save!
      end
      redirect_to songs_path
    end
