12cfd1c9a97c77c2adcfaeaebbbb28719cc9e39f
github username <github email address>
2019-06-11 15:57:19 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,10 +45,16 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+  def upload
+    CSV.foreach(params[:file].path, headers: true) do |lead|
+      Song.create(title: lead[0], artist_id: Artist.find_or_create_by(name: lead[1]).id)
+    end
+    redirect_to songs_path
+  end
+
   private
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
