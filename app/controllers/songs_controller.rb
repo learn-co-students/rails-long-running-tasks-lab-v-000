214a1e315833b0f6407b8345d06ec09eabdc40f6
brennanmrose <brennan.m.rose@gmail.com>
2019-06-16 17:11:39 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,16 @@
 class SongsController < ApplicationController
+  require 'csv'
 
   def index
     @songs = Song.all
+  end
+
+  def upload
+    CSV.foreach(params[:file].path, headers: true) do |song|
+      @artist = Artist.find_or_create_by(name: song[1])
+      Song.create(title: song[0], artist_id: @artist.id)
+    end
+    redirect_to songs_path
   end
 
   def show
@@ -48,7 +57,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :fixture_file_upload)
   end
 end
 
